da979324bcec5a60068909f7ea5417b83bc8dc81
kianlew <kianlew88@gmail.com>
2025-03-07 04:15:49 +0800

    terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,27 +1,27 @@
-resource "aws_s3_bucket" "template_project_data" {
-  bucket = "template-project-data"
+resource "aws_s3_bucket" "project_data_bucket" {
+  bucket = "bmin5100-kianlew"
 
   tags = {
     Owner = element(split("/", data.aws_caller_identity.current.arn), 1)
   }
 }
 
-resource "aws_s3_bucket_ownership_controls" "template_project_data_ownership_controls" {
-  bucket = aws_s3_bucket.template_project_data.id
+resource "aws_s3_bucket_ownership_controls" "project_data_bucket_ownership_controls" {
+  bucket = aws_s3_bucket.project_data_bucket.id
   rule {
     object_ownership = "BucketOwnerPreferred"
   }
 }
 
-resource "aws_s3_bucket_acl" "template_project_data_acl" {
-  depends_on = [aws_s3_bucket_ownership_controls.template_project_data_ownership_controls]
+resource "aws_s3_bucket_acl" "project_data_bucket_acl" {
+  depends_on = [aws_s3_bucket_ownership_controls.project_data_bucket_ownership_controls]
 
-  bucket = aws_s3_bucket.template_project_data.id
+  bucket = aws_s3_bucket.project_data_bucket.id
   acl    = "private"
 }
 
-resource "aws_s3_bucket_lifecycle_configuration" "template_project_data_expiration" {
-  bucket = aws_s3_bucket.template_project_data.id
+resource "aws_s3_bucket_lifecycle_configuration" "project_data_bucket_expiration" {
+  bucket = aws_s3_bucket.project_data_bucket.id
 
   rule {
     id      = "compliance-retention-policy"
